16c469de4a37fda740c864c662067890e08582dc
Roberto Carlos <128102241+robertocarlos1007@users.noreply.github.com>
2024-10-21 18:55:40 -0300

    Add files via upload

---
+++ exercicios/aula3/aula3.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+#Aspas Simples
+print('Olá!')
+
+#Aspas Duplas
+print("Olá!")
+
+#Escape
+print("Olá\"Mundo\"")
+
+# r
+print(r"Olá \'Mundo\'")
+
+# Uma forma mais simples em vez de utilizar o escape ou r.
+print("Olá 'Mundo'")
