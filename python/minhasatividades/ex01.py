a7ea1a7859167ac5e0cb45efd272cb35df88435a
Roberto Carlos <128102241+robertocarlos1007@users.noreply.github.com>
2024-11-23 10:47:37 -0300

    Add files via upload

---
+++ python/minhasatividades/ex01.py
@@ -0,0 +1,10 @@
+"""
+Descrição: Escreva um programa que peça ao usuário para digitar três notas, calcule a média e informe se o aluno foi aprovado ou reprovado.
+
+Critérios:
+
+A média para aprovação é 7.0.
+Exemplo:
+yaml
+Copiar código
+"""
