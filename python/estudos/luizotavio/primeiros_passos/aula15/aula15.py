0e88ea0528df8a424f3e66a6f67f612200cc6d3a
Roberto Carlos <128102241+robertocarlos1007@users.noreply.github.com>
2024-11-29 19:53:06 -0300

    Add files via upload

---
+++ python/estudos/luizotavio/primeiros_passos/aula15/aula15.py
@@ -0,0 +1,6 @@
+nome = input("Qual o seu nome? ")
+print(f"Olá, {nome}! Que nome massa!")
+num1 = int(input("Digite um número:"))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+print(soma)
