0b03bf82023e05603124c802ef66dda44ec2bf99
Roberto Carlos <128102241+robertocarlos1007@users.noreply.github.com>
2024-12-02 18:40:47 -0300

    Add files via upload

---
+++ python/estudos/luizotavio/primeiros_passos/aula16/aula16.py
@@ -0,0 +1,9 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input("Você quer entrar ou sair? ")
+if entrada == 'entrar':
+    print("VOCÊ ENTROU NO SISTEMA!")
+elif entrada == 'sair':
+    print("VOCÊ SAIU DO SISTEMA!")
+else:
+    print("Você nem saiu e nem entrou no sistema!")
